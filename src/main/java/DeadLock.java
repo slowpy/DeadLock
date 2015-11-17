ae2ee58870884f4ef2671a100cd2cdc3bc3b8ab8
hirdil <laszlohirdi@gmail.com>
2015-11-17 12:10:09 +0100

    renaming

--- src/main/java/DeadLock.java
+++ src/main/java/DeadLock.java
@@ -4,23 +4,23 @@ public class DeadLock {
 	/**
 	 * States to go through to reproduce the deadlock precondition.
 	 */
-	private enum DeadLockPreconditionStateMachineState {
+	private enum PreconditionState {
 		NO_LOCKS, THREAD1_LOCKS_RESOURCE1, THREAD1_LOCKS_RESOURCE1__AND__THREAD2_LOCKS_RESOURCE2
 	}
 
-	private transient DeadLockPreconditionStateMachineState _preconditionState = DeadLockPreconditionStateMachineState.NO_LOCKS;
+	private transient PreconditionState _preconditionState = PreconditionState.NO_LOCKS;
 	private final String _resource1 = "RESOURCE1";
 	private final String _resource2 = "RESOURCE2";
 
 	private final Thread _thread1 = new Thread("Thread1") {
 		public void run() {
-			if (_preconditionState != DeadLockPreconditionStateMachineState.NO_LOCKS) {
+			if (_preconditionState != PreconditionState.NO_LOCKS) {
 				logBadStateError(this.getName(), _preconditionState);
 				return;
 			}
 			synchronized (_resource1) {
-				_preconditionState = DeadLockPreconditionStateMachineState.THREAD1_LOCKS_RESOURCE1;
-				while (_preconditionState != DeadLockPreconditionStateMachineState.THREAD1_LOCKS_RESOURCE1__AND__THREAD2_LOCKS_RESOURCE2) {
+				_preconditionState = PreconditionState.THREAD1_LOCKS_RESOURCE1;
+				while (_preconditionState != PreconditionState.THREAD1_LOCKS_RESOURCE1__AND__THREAD2_LOCKS_RESOURCE2) {
 					logBadStateError(this.getName(), _preconditionState);
 				}
 				synchronized (_resource2) {
@@ -33,11 +33,11 @@ public class DeadLock {
 
 	private final Thread _thread2 = new Thread("Thread2") {
 		public void run() {
-			while (_preconditionState != DeadLockPreconditionStateMachineState.THREAD1_LOCKS_RESOURCE1) {
+			while (_preconditionState != PreconditionState.THREAD1_LOCKS_RESOURCE1) {
 				logBadStateError(this.getName(), _preconditionState);
 			}
 			synchronized (_resource2) {
-				_preconditionState = DeadLockPreconditionStateMachineState.THREAD1_LOCKS_RESOURCE1__AND__THREAD2_LOCKS_RESOURCE2;
+				_preconditionState = PreconditionState.THREAD1_LOCKS_RESOURCE1__AND__THREAD2_LOCKS_RESOURCE2;
 				synchronized (_resource1) {
 					logBothResourcesAquired(this.getName());
 				}
@@ -45,7 +45,7 @@ public class DeadLock {
 		}
 	};
 
-	private static void logBadStateError(final String threadName, final DeadLockPreconditionStateMachineState state) {
+	private static void logBadStateError(final String threadName, final PreconditionState state) {
 		System.err.println(threadName + " bad state: " + state);
 	}
 
